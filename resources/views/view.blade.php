dccd21cba93c5efb43f80815b61ecd46ae325fea
thiagousa <thiago@thiagodsantos.com>
2018-06-14 15:34:38 -0400

    Add bootstrap

--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -2,7 +2,7 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
     <head>
-            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+            <link rel="stylesheet" href="{{{ URL::asset('css/bootstrap.min.css')}}}">
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -19,6 +19,7 @@
                         <th scope="col">Quantity</th>
                         <th scope="col" >Price Buy</th>
                         <th scope="col" >Price Sale</th>
+                        <link rel="stylesheet"  />
                       
                     </tr>
                     </thead>
