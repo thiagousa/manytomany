b16549d47ebe4e7813c77a87e051c8e875089e53
Samuel Gomes <hellish.doom@gmail.com>
2018-06-16 02:06:10 -0300

    Remove unused import

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -1,34 +1,23 @@
 <?php
 
-
 namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
 
 use App\Orders;
 use App\Products;
 use App\Promoters;
-use App\Orders_Products_Promotors;
 
 class OrdersController extends Controller
 {
-   
     public function getIndex()
     {
-       
+        $orders = orders::with(['promoters', 'products'])->get();
 
-        $orders = orders::with(['promoters','products'])->get();
-        
         return view('index')->with(compact('orders'));
-
-
     }
-
 
     public function getView($ordersId)
     {
-          
-        $orders = orders::find($ordersId)->products()->orderBy('productsID')->withPivot(['o_quantity','o_priceBegin','o_priceEnd'])->get();
+        $orders = orders::find($ordersId)->products()->orderBy('productsID')->withPivot(['o_quantity', 'o_priceBegin', 'o_priceEnd'])->get();
 
         return view('view')->with(compact('orders'));
     }
