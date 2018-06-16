1f9798133e64080a1983354b2e5698b0307caf11
Samuel Gomes <hellish.doom@gmail.com>
2018-06-16 02:33:50 -0300

    html fix format

--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -1,38 +1,36 @@
-
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        
         <title>Laravel</title>
-      
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">  
     </head>
     <body>
-            <table class="table">
-                    <thead  class="thead-dark">
+    <div class="container">
+        <table class="table">
+            <thead  class="thead-dark">
+                <tr>
+                    <th>Id</th>
+                    <th>Description</th>
+                    <th>Quantity</th>
+                    <th >Price Buy</th>
+                    <th >Price Sale</th>
+                </tr>
+            </thead>
+            <tbody>   
+                @foreach($orders as $order)
                     <tr>
-                        <th scope="col">Id</th>
-                        <th scope="col">Description</th>
-                        <th scope="col">Quantity</th>
-                        <th scope="col" >Price Buy</th>
-                        <th scope="col" >Price Sale</th>
-                        <link rel="stylesheet"  />
-                      
+                        <td>{{ $order->productsId}}</td>
+                        <td>{{ $order->description }}</td>
+                        <td>{{$order->pivot->o_quantity}}</td>
+                        <td>{{$order->pivot->o_priceBegin}}</td>
+                        <td>{{$order->pivot->o_priceEnd}}</td>
                     </tr>
-                    </thead>
-                    <tbody>
-                           
-                        @foreach($orders as $order)
-                            <th scope="row">{{ $order->productsId}}</th>
-                            <th scope="row" >{{ $order->description }}</th>
-                            <th scope="row" >{{$order->pivot->o_quantity}}</th>
-                            <th scope="row" >{{$order->pivot->o_priceBegin}}</th>
-                            <th scope="row" >{{$order->pivot->o_priceEnd}}</th>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
     </body>
 </html>
