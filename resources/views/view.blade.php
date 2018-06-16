3e9b9f7f7d9ff68080ee370848747f1474af9d8c
Samuel Gomes <hellish.doom@gmail.com>
2018-06-16 02:27:09 -0300

    Remove css link

--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -2,7 +2,6 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
     <head>
-            <link rel="stylesheet" href="{{{ URL::asset('css/bootstrap.min.css')}}}">
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
