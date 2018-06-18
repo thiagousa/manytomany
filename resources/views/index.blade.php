206ae21f6cdb7e2e6bd0198fb46cd0fcb028cb81
thiagousa <thiago@thiagodsantos.com>
2018-06-18 11:37:05 -0400

    Chande etails dor Details

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -29,7 +29,7 @@
                         <td >{{ $order->status}}</td>
                         <td>{{ $order->updated_at->format('m/d/Y')}}</td>
                         <td>
-                            <a href="{{ route('orders.show', [ 'id' => $order->ordersId ])}}">View etails</a>
+                            <a href="{{ route('orders.show', [ 'id' => $order->ordersId ])}}">View Details</a>
                         </td>
                     </tr>
                     @endforeach
